e2a50b4f1874ab00a56667195ee22a2ef1aa3e73
Camilo Diaz <camilo.diaz@avaldigitallabs.com>
2020-05-26 21:20:51 -0500

    upload images

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -4,6 +4,8 @@ const app = express();
 
 const User = require('../models/User');
 const Product = require('../models/Product');
+const fs = require('fs');
+const path = require('path');
 
 app.use(fileUpload());
 
@@ -56,20 +58,58 @@ app.put('/upload/:type/:id', (req, res) => {
                 ok: false,
                 err
             });
-        userImage(id, res, fileName);
-        //Update img
+        if (type === validTypes[1]) {
+            updateUserImgName(id, res, fileName);
+        } else {
+            updateProductImage(id, res, fileName);
+        }
     });
 });
 
-const userImage = (id, res, fileName) => {
-    User.findById(id, (err, userDB) => {
+const updateProductImage = (id, res, fileName) => {
+    Product.findById(id, (err, product) => {
+        const type = 'products';
         if (err) {
+            deleteImage(fileName, type);
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+        if (!product) {
+            deleteImage(fileName, type);
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Product does not exist'
+                }
+            });
+        };
+        if (product.img) {
+            deleteImage(product.img, type);
+        }
+        product.img = fileName;
+        product.save((err, newProduct) => {
+            res.json({
+                ok: true,
+                product: newProduct
+            });
+        });
+    });
+};
+
+const updateUserImgName = (id, res, fileName) => {
+    User.findById(id, (err, userDB) => {
+        const type = 'users';
+        if (err) {
+            deleteImage(fileName, type);
             return res.status(500).json({
                 ok: false,
                 err
             });
         };
         if (!userDB) {
+            deleteImage(fileName, type);
             return res.status(400).json({
                 ok: false,
                 err: {
@@ -77,6 +117,7 @@ const userImage = (id, res, fileName) => {
                 }
             });
         };
+        deleteImage(userDB.img, type);
         userDB.img = fileName;
         userDB.save((err, userDB) => {
             res.json({
@@ -86,7 +127,13 @@ const userImage = (id, res, fileName) => {
             });
         });
     });
-}
+};
 
+const deleteImage = (imageName, type) => {
+    let pathImagen = path.resolve(__dirname, `../../uploads/${type}/${ imageName }`);
+    if (fs.existsSync(pathImagen)) {
+        fs.unlinkSync(pathImagen);
+    };
+};
 
 module.exports = app;
